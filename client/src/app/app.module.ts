d5e51e7f4716bd4f05e75d56ebb810aa41202c2b
Chiho Maekawa <mokochii1108@gmail.com>
2023-02-07 19:12:05 -0800

    Added pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ResisterComponent } from './pages/resister/resister.component';
+import { SetupComponent } from './pages/setup/setup.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ResisterComponent,
+    SetupComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
