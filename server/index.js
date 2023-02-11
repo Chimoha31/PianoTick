bf2eddf628bf6178ea01d1cdf560550cc54c219c
Chiho Maekawa <mokochii1108@gmail.com>
2023-02-10 23:28:25 -0800

    Installed jwt

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
 const PORT = 5000;
 
 app.get("/", (eq, res) => {
   res.send("Hello server");
 });
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Successfully connected to mongoDB 🎉")
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT " + PORT + "🌍");
